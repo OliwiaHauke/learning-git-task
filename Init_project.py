00c4956069a985a2ee8468d210098b125c5b9f5a
Oliwia Hauke <oliwia.urbanska.95@gmail.com>
2025-01-13 17:54:27 +0100

    Add kiosk

--- Init_project.py
+++ Init_project.py
@@ -2,10 +2,11 @@ shopping_list = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"],
     "apteka": ["witamina C", "elektrolity"],
-    "lidl": ["pinsa", "salami"]
+    "lidl": ["pinsa", "salami"],
+    "kiosk": ["bilet autobusowy"]
 }
 for shop, products in shopping_list.items():
     products_capitalized = [product.capitalize() for product in products]
     print(f"Idę do {shop.capitalize()}, kupuję tam następujące rzeczy: {', '.join(products_capitalized)}")
 total_items = sum(len(items) for items in shopping_list.values())
-print(f"W sumie kupuję {total_items} produktów.") 
+print(f"W sumie kupuję {total_items} produktów.")
